df049b83418376547db1c315ffd82e10374d6d04
HajarBENDHIBA <hbendhiba46@gmail.com>
2024-12-30 14:03:34 +0100

    update 1

--- src/Component/MovieCard.jsx
+++ src/Component/MovieCard.jsx
@@ -1,15 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const MovieCard = ({ movie }) => {
   return (
-    <div className="border rounded-lg p-4 shadow-md">
-      {/* Increase the height and width for a larger image */}
-      <img 
-        src={movie.posterURL} 
-        alt={movie.title} 
-        className="w-full h-80 object-cover rounded-md"  // Adjusted height to 80
-      />
+    <div className="border rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow duration-200">
+      <img src={movie.posterURL}  alt={movie.title} className="w-full h-80 object-cover rounded-md" />
       <h2 className="text-lg font-bold mt-2">{movie.title}</h2>
-      <p className="text-gray-600">{movie.description}</p>
+      <p className="text-gray-600">{movie.description.slice(0, 100)}...</p>
       <p className="text-yellow-500 font-semibold">Rating: {movie.rating}</p>
+      <Link to={`/movie/${movie.title}`} className="inline-block mt-4 text-blue-500"> View Details </Link>
     </div>
   );
 };
--- src/Component/MovieList.jsx
+++ src/Component/MovieList.jsx
@@ -1,23 +1,14 @@
+import MovieCard from './MovieCard';
+
 const MovieList = ({ movies }) => {
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {movies.map((movie, index) => (
-        <div
-          key={index}
-          className="bg-white border border-gray-300 rounded-lg shadow p-4 flex flex-col items-center"
-        >
-          <img
-            src={movie.posterURL}
-            alt={movie.title}
-            className="w-48 object-cover rounded-lg"
-          />
-          <h2 className="text-lg font-bold mt-2 text-center">{movie.title}</h2>
-          <p className="text-sm text-gray-600 mt-1 text-center">{movie.description}</p>
-          <p className="text-yellow-500 font-bold mt-2">Rating: {movie.rating}</p>
-        </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      {movies.map((movie) => (
+        <MovieCard key={movie.title} movie={movie} />
       ))}
     </div>
   );
 };
 
 export default MovieList;
+ 
